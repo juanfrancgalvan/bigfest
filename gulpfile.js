a4afcf80c2f644bb6aa65d61bcb4d16d28f8cbf5
Juan Francisco <juanfrancgalvan@gmail.com>
2024-02-29 14:46:48 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const { src, dest, watch, parallel } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const plumber = require('gulp-plumber')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+const terser = require('gulp-terser-js')
+
+function css(done) {
+  src('source/styles/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/styles'))
+  done()
+}
+
+function js(done) {
+  src('source/scripts/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/scripts'))
+  done()
+}
+
+function images(done) {
+  const options = {optimizationLevel: 5, quality: 50}
+  src('source/assets/images/**/*')
+    .pipe(imagemin(options))
+    .pipe(webp())
+    .pipe(dest('build/assets/images'))
+  done()
+}
+
+function dev(done) {
+  watch("source/styles/**/*.scss", css)
+  watch("source/scripts/**/*.js", js)
+  done()
+}
+
+exports.css = css
+exports.js = js
+exports.images = images
+
+exports.dev = parallel(css, js, dev)
+exports.convert = parallel(images)
---
+++ source/scripts/script.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function () {
+  start()
+})
+
+function start() {
+  headerFixed()
+  smoothScroll()
+  createGallery()
+}
+
+function headerFixed() {
+  const body = document.querySelector('body')
+  const header = document.querySelector('.header')
+  const festival = document.querySelector('.festival')
+
+  window.addEventListener('scroll', function () {
+    if (festival.getBoundingClientRect().bottom < 0) {
+      header.classList.add('fixed')
+      body.classList.add('body-scroll')
+    }
+    else {
+      header.classList.remove('fixed')
+      body.classList.remove('body-scroll')
+    }
+  })
+}
+
+function smoothScroll() {
+  const links = document.querySelectorAll('.nav a')
+  links.forEach(enlace => {
+    enlace.addEventListener('click', function (e) {
+      e.preventDefault()
+      const scrollSection = e.target.attributes.href.value
+      const section = document.querySelector(scrollSection)
+      section.scrollIntoView({ behavior: 'smooth' })
+    })
+  })
+}
+
+function createGallery() {
+  const gallery = document.querySelector('.gallery-images')
+  for (let i = 1; i <= 12; i++) {
+    const image = document.createElement('li')
+    image.innerHTML = `<img loading="lazy" width="200" height="300" src="build/assets/images/thumb/${i}.webp" alt="Gallery image">`
+    image.onclick = function () {
+      showImage(i)
+    }
+    gallery.appendChild(image)
+  }
+}
+
+function showImage(id) {
+  const image = document.createElement('li')
+  image.innerHTML = `<img loading="lazy" width="200" height="300" src="source/assets/images/original/${id}.jpg" alt="Gallery image">`
+  const overlay = document.createElement('div')
+  overlay.appendChild(image)
+  overlay.classList.add('overlay')
+  overlay.onclick = function () {
+    const body = document.querySelector('body')
+    body.classList.remove('fix')
+    overlay.remove()
+  }
+
+  const body = document.querySelector('body')
+  body.appendChild(overlay)
+  body.classList.add('fix')
+
+  const closeImage = document.createElement('p')
+  closeImage.textContent = 'X'
+  closeImage.classList.add('close-button')
+  closeImage.onclick = function () {
+    const body = document.querySelector('body')
+    body.classList.remove('fix')
+    overlay.remove()
+  }
+  overlay.appendChild(closeImage)
+}
