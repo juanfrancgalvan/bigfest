3460c87ce69a5b08668c51cc45640293256e3885
Juan Francisco <juanfrancgalvan@gmail.com>
2026-03-19 12:54:11 -0300

    modified:   build/scripts/script.js 	modified:   build/scripts/script.js.map 	modified:   build/styles/style.css 	modified:   build/styles/style.css.map 	modified:   source/scripts/script.js 	modified:   source/styles/base/_globals.scss 	modified:   source/styles/base/_index.scss 	new file:   source/styles/base/_typography.scss

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,34 +1,33 @@
 function createGallery() {
-  const gallery = document.querySelector('.gallery-images')
+  const gallery = document.querySelector('.gallery-images');
   for (let i = 1; i <= 12; i++) {
-    const image = document.createElement('img')
-    image.src = `build/assets/images/thumb/${i}.webp`
-    image.alt = `Show ${i}`
-    image.width = 200
-    image.height = 300
-    image.loading = 'lazy'
-    gallery.appendChild(image)
+    const image = document.createElement('img');
+    image.src = `build/assets/images/thumb/${i}.webp`;
+    image.alt = `Show ${i}`;
+    image.width = 200;
+    image.height = 300;
+    image.loading = 'lazy';
+    gallery.appendChild(image);
 
     image.addEventListener('click', function showImage() {
-      const body = document.querySelector('body')
-      const overlay = document.createElement('div')
-      const image = document.createElement('img')
-      const header = document.querySelector('.header')
-      overlay.classList.add('overlay')
-      header.classList.remove('fixed')
-      image.src = `source/assets/images/original/${i}.jpg`
-      image.alt = `Show ${i}`
-      image.width = 200
-      image.height = 300
-      image.loading = 'lazy'
-      body.appendChild(overlay)
-      overlay.appendChild(image)
+      const body = document.querySelector('body');
+      const overlay = document.createElement('div');
+      const image = document.createElement('img');
+      const header = document.querySelector('.header');
+      overlay.classList.add('overlay');
+      header.classList.remove('fixed');
+      image.src = `source/assets/images/original/${i}.jpg`;
+      image.alt = `Show ${i}`;
+      image.width = 200;
+      image.height = 300;
+      image.loading = 'lazy';
+      body.appendChild(overlay);
+      overlay.appendChild(image);
 
       overlay.addEventListener('click', function closeImage() {
-        overlay.remove()
-        header.classList.add('fixed')
-      })
-    })
+        overlay.remove();
+      });
+    });
   }
 }
-createGallery()
+createGallery();
